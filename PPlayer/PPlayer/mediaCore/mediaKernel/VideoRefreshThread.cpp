90cc0ff2a8bf65dc4441a288b92b2937902b24e1
邱开禄 <944624367@qq.com>
2019-11-20 22:03:09 +0800

    [feature] Initially add video refresh thread
    1. Initially add video refresh thread

--- PPlayer/PPlayer/mediaCore/mediaKernel/VideoRefreshThread.cpp
+++ PPlayer/PPlayer/mediaCore/mediaKernel/VideoRefreshThread.cpp
@@ -8,7 +8,33 @@
 #include "VideoRefreshThread.h"
 
 NS_MEDIA_BEGIN
+SDL_mutex *VideoRefreshThread::mutex = SDL_CreateMutex();      //类的静态指针需要在此初始化
+VideoRefreshThread* VideoRefreshThread::p_VideoOut = nullptr;
 
+VideoRefreshThread::VideoRefreshThread()
+{
+    
+}
+
+VideoRefreshThread::~VideoRefreshThread()
+{
+    
+}
+
+void VideoRefreshThread::init()
+{
+    
+}
+
+void VideoRefreshThread::start()
+{
+    
+}
+
+void VideoRefreshThread::run()
+{
+    
+}
 
 
 NS_MEDIA_END
--- PPlayer/PPlayer/mediaCore/mediaKernel/VideoRefreshThread.h
+++ PPlayer/PPlayer/mediaCore/mediaKernel/VideoRefreshThread.h
@@ -9,8 +9,38 @@
 #ifndef VideoRefreshThread_H
 #define VideoRefreshThread_H
 #include "MediaDefineInfo.h"
+#include <thread>
+
 NS_MEDIA_BEGIN
 
+class VideoRefreshThread : public std::thread
+{
+public:
+    static VideoRefreshThread *getIntanse()  // 饿汉模式
+    {
+        if(NULL == p_VideoOut) {
+            SDL_LockMutex(mutex);
+            if(NULL == p_VideoOut) {
+                p_VideoOut = new (std::nothrow)VideoRefreshThread();
+                if(p_VideoOut == NULL) {
+                    printf("DemuxThread getInstance is NULL!");
+                }
+            }
+            SDL_UnlockMutex(mutex);
+        }
+        return p_VideoOut;
+    }
+    void init();
+    void start();
+    void run();
+    
+    VideoRefreshThread();
+    virtual ~VideoRefreshThread();
+private:
+    PlayerContext *pPlayerContext;
+    static VideoRefreshThread* p_VideoOut;
+    static SDL_mutex *mutex;
+};
 
 
 NS_MEDIA_END

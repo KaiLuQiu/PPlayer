f9643a421fb245188860514e2fa2c51902c02f74
邱开禄 <944624367@qq.com>
2019-12-08 10:55:13 +0800

    [feature] Modify naming
    1. Modify naming

--- PPlayer/PPlayer/mediaCore/mediaKernel/messageQueue.cpp
+++ PPlayer/PPlayer/mediaCore/mediaKernel/messageQueue.cpp
@@ -9,55 +9,50 @@
 
 message::message()
 {
-    Queue = new std::list<MessageCmd>();
-    if (Queue == NULL)
-    {
+    messageQueue = new std::list<MessageCmd>();
+    if (messageQueue == NULL) {
+        printf("message Queue is NULL!!!")
     }
 }
 
 message::~message()
 {
-    if (Queue != NULL)
-    {
-        Queue->clear();
-        delete Queue;
+    if (messageQueue != NULL) {
+        messageQueue->clear();
+        delete messageQueue;
     }
 }
 
 int message::message_queue(MessageCmd cmd)
 {
     mutex.lock();
-    Queue->push_back(cmd);
+    messageQueue->push_back(cmd);
     mutex.unlock();
-    
     return true;
 }
 
 int message::message_dequeue(MessageCmd *cmd)
 {
     mutex.lock();
-    if (!Queue->empty())
-    {
-        *cmd = Queue->front();
-        Queue->pop_front();
+    if (!messageQueue->empty()) {
+        *cmd = messageQueue->front();
+        messageQueue->pop_front();
     }
-    else
-    {
+    else {
         mutex.unlock();
         return false;
     }
     mutex.unlock();
-    
     return true;
 }
 
 int message::message_cmd_size()
 {
-    return Queue->size();
+    return messageQueue->size();
 }
 
 int message::message_is_empty()
 {
-    return Queue->empty();
+    return messageQueue->empty();
 }
 
--- PPlayer/PPlayer/mediaCore/mediaKernel/messageQueue.hpp
+++ PPlayer/PPlayer/mediaCore/mediaKernel/messageQueue.hpp
@@ -11,38 +11,19 @@
 #include <list>
 #include <mutex>
 
-enum {
+typedef enum {
     MESSAGE_CMD_NONE,
     MESSAGE_CMD_PAUSE,
     MESSAGE_CMD_RESUME,
     MESSAGE_CMD_SEEK,
     MESSAGE_CMD_STOP,
-    MESSAGE_CMD_SEL_TRACK,
     MESSAGE_CMD_FLUSH,
-    MESSAGE_CMD_DECODE,
-    MESSAGE_CMD_DMX,
-    MESSAGE_CMD_WAIT_RBUF,
     MESSAGE_CMD_REINIT,
     MESSAGE_CMD_RESYNC,
     MESSAGE_CMD_REINIT_AUD_DRV,
-    MESSAGE_CMD_RENDER,
-    MESSAGE_CMD_INPUT_TYPE_CHANGE,
     MESSAGE_CMD_FORCE_EOF,
     MESSAGE_CMD_CHANGE_SPEED,
-};
-
-typedef enum
-{
-    MESSAGE_CMD_QUEUE = 0,
-    MESSAGE_NOTIFY_QUEUE,
-    MESSAGE_QUEUE_TYPES
-} MessageQueueType;
-
-typedef struct
-{
-    MessageQueueType cmdType;
-    int cmd;
-} MessageCmd;
+}MessageCmd;
 
 class message
 {
@@ -55,7 +36,7 @@ public:
     int message_is_empty();
 private:
     std::mutex mutex;
-    std::list<MessageCmd> *Queue;
+    std::list<MessageCmd> *messageQueue;
 };
 
 
